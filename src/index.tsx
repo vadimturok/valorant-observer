7ecaf80c8caed54dd7d3114c21fd0d69341609f4
Vadym <turokvadim2510@gmail.com>
2022-05-02 14:54:51 +0300

    gh-pages deploy prep

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import Layout from "./components/Layout/Layout";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <Layout>
         <App />
       </Layout>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
